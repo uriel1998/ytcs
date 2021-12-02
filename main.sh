dcf98efdae2632961af41a156a42111f25c8dd86
Steven Saus <steven@stevesaus.com>
2021-12-02 05:31:10 -0500

    Initial setup

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+# init variables
+# get INI
+# parse args
+
+
+do while
+    get_subscriptions
+        choose_subscription
+    merge cache
+    choose_video
+    play_video
+done
